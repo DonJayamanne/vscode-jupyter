8f03099aaca262894a9d3857c9c236dae2b705cb
Don Jayamanne <don.jayamanne@yahoo.com>
2021-03-08 08:57:39 -0800

    Misc

--- src/client/datascience/notebook/cells/cellLanguage.ts
+++ src/client/datascience/notebook/cells/cellLanguage.ts
@@ -1,9 +1,11 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import { injectable } from 'inversify';
-import { NotebookCellData, window, workspace, WorkspaceEdit } from 'vscode';
+import { inject, injectable } from 'inversify';
+import { languages, notebook, NotebookCellData, window, workspace, WorkspaceEdit } from 'vscode';
 import { IExtensionSyncActivationService } from '../../../activation/types';
+import { IDisposableRegistry } from '../../../common/types';
+import { isJupyterKernel } from '../helpers/helpers';
 
 const CellMagicLanguages = new Map<string, string>([
     ['html', 'html'],
@@ -23,43 +25,37 @@ const CellMagicLanguages = new Map<string, string>([
 ]);
 @injectable()
 export class CellMagicMonitor implements IExtensionSyncActivationService {
-    // constructor(){}
+    constructor(@inject(IDisposableRegistry) private readonly disposables: IDisposableRegistry) {}
     activate(): void {
-        workspace.onDidChangeTextDocument(async (e) => {
-            console.log(e?.document.uri);
-
-            if (!e?.document.notebook) {
-                return;
-            }
-            const cell = e.document.notebook.cells.find((item) => item.document === e.document);
-            if (!cell) {
-                return;
-            }
-            const cellMagic = e.document.lineAt(0).text.trim();
-            if (!cellMagic.startsWith('%%')) {
-                return;
-            }
-            const magic = cellMagic.substring(2).trim();
-            const newLanguage = CellMagicLanguages.get(magic);
-            if (!newLanguage) {
-                return;
-            }
-            if (e.document.languageId.toLowerCase() === newLanguage.toLowerCase()) {
-                return;
-            }
-            const edit = new WorkspaceEdit();
-            edit.replaceNotebookCellMetadata(e.document.uri, cell.index, cell.metadata.with({}));
-            edit.replaceNotebookCells(cell.notebook.uri, cell.index, cell.index + 1, [
-                new NotebookCellData(
-                    cell.cellKind,
-                    cell.document.getText(),
-                    newLanguage,
-                    [...cell.outputs],
-                    cell.metadata
-                )
-            ]);
-            await workspace.applyEdit(edit);
-        });
-        // notebook.onDidChangeCellLanguage
+        workspace.onDidChangeTextDocument(
+            async (e) => {
+                if (
+                    !e?.document.notebook ||
+                    window.activeNotebookEditor?.document !== e.document.notebook ||
+                    (window.activeNotebookEditor?.kernel && !isJupyterKernel(window.activeNotebookEditor?.kernel))
+                ) {
+                    return;
+                }
+                const cell = e.document.notebook.cells.find((item) => item.document === e.document);
+                if (!cell) {
+                    return;
+                }
+                const cellMagic = e.document.lineAt(0).text.trim();
+                if (!cellMagic.startsWith('%%')) {
+                    return;
+                }
+                const magic = cellMagic.substring(2).trim();
+                const newLanguage = CellMagicLanguages.get(magic);
+                if (!newLanguage) {
+                    return;
+                }
+                if (e.document.languageId.toLowerCase() === newLanguage.toLowerCase()) {
+                    return;
+                }
+                await languages.setTextDocumentLanguage(e.document, newLanguage);
+            },
+            this,
+            this.disposables
+        );
     }
 }
